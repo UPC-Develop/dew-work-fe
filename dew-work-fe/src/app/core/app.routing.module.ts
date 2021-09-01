46c31fb2a595caf9c9fbbd9c3c5f7c9e919cb1de
jcajasba <joel.cajas@telefonica.com>
2021-09-01 18:59:23 -0500

    login de usuario y shopping cart

--- dew-work-fe/src/app/core/app.routing.module.ts
+++ dew-work-fe/src/app/core/app.routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactoComponent } from '../contacto/contacto.component';
 
 import { InicioComponent } from '../inicio/inicio.component';
+import { LoginComponent } from '../login/login.component';
 import { AboutusComponent } from '../pages/aboutus/aboutus.component';
 import { CatalogoComponent } from '../pages/catalogo/catalogo.component';
 import { OfertaComponent } from '../pages/oferta/oferta.component';
 import { ProductComponent } from '../pages/product/product.component';
+import { ShoppingCartComponent } from '../shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent},
@@ -15,6 +17,8 @@ const routes: Routes = [
   { path: 'catalogo', component: CatalogoComponent },
   { path: 'oferta', component: OfertaComponent },
   { path: 'product/:id', component: ProductComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
   { path: '**', redirectTo: '/inicio', pathMatch: 'full' }
 ];
 
