827f9fec7344b0338b09a55fd2ef7be336783266
Josue Torre Quispe <jtorre@asf.pe>
2021-09-01 18:21:03 -0500

    se agrego inicio y contacto

--- dew-work-fe/src/app/core/app.routing.module.ts
+++ dew-work-fe/src/app/core/app.routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactoComponent } from '../contacto/contacto.component';
 
-import { HomeComponent } from '../home/home.component';
+import { InicioComponent } from '../inicio/inicio.component';
 import { AboutusComponent } from '../pages/aboutus/aboutus.component';
 import { CatalogoComponent } from '../pages/catalogo/catalogo.component';
 import { OfertaComponent } from '../pages/oferta/oferta.component';
 import { ProductComponent } from '../pages/product/product.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'inicio', component: InicioComponent},
+  { path: 'contacto', component: ContactoComponent},
   { path: 'aboutus', component: AboutusComponent },
   { path: 'catalogo', component: CatalogoComponent },
   { path: 'oferta', component: OfertaComponent },
   { path: 'product/:id', component: ProductComponent },
-  { path: '**', redirectTo: '/home', pathMatch: 'full' }
+  { path: '**', redirectTo: '/inicio', pathMatch: 'full' }
 ];
 
 @NgModule({
